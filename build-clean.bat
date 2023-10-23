ee12e2039d95afb13199069a28d65176ff49bc60
Ben Siebert <hello@ben-siebert.de>
2023-10-23 13:03:58 +0200

    Update

---
+++ build-clean.bat
@@ -0,0 +1,5 @@
+
+rmdir /S /Q pdf
+
+"C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory pdf -job-name "Projektarbeit" tex/entry.tex
+"C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory pdf -job-name "Projektarbeit" tex/entry.tex
