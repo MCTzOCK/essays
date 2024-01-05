b0764edcf0d9d3e9fa3df1569ef14abef3933933
Ben Siebert <hello@ben-siebert.de>
2024-01-05 12:21:22 +0100

    Update

---
+++ 2023_2024-Q1/Erdkunde/landwirtschaft/29-10-2023-landkäufe-Ernährungssicherheit/build.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory pdf -job-name "Landkaeufe als Beitrag zur Ernaehrungssicherheit" tex/entry.tex
+"C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory pdf -job-name "Landkaeufe als Beitrag zur Ernaehrungssicherheit" tex/entry.tex
