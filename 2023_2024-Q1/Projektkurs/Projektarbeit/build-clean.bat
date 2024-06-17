3a0127af7cdade26ed50c3bf454c99ea25951f17
Ben Siebert <hello@ben-siebert.de>
2024-06-17 19:07:13 +0200

    Update

--- 2023_2024-Q1/Projektkurs/Projektarbeit/build-clean.bat
+++ 2023_2024-Q1/Projektkurs/Projektarbeit/build-clean.bat
@@ -6,6 +6,7 @@ cd src
 "C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory ../pdf -job-name "Projektarbeit" entry.tex
 biber ../pdf/Projektarbeit
 "C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory ../pdf -job-name "Projektarbeit" entry.tex
+biber ../pdf/Projektarbeit
 "C:\Program Files\MiKTeX\miktex\bin\x64\pdflatex.exe" -output-directory ../pdf -job-name "Projektarbeit" entry.tex
 
 cd ..
